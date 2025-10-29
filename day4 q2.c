c8ae6aea010c116089833579835f3d6788d6478c
shivani2007shivani-lab <shivani2007shivani@gmail.com>
2025-10-29 07:47:39 +0530

    Add files via upload

---
+++ day4 q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int num1,num2;
+	printf("enter first number :");
+	scanf("%d",&num1);
+	printf("enter second number :");
+	scanf("%d",&num2);
+	if(num1==num2){
+		printf("true\n");
+	}else{
+		printf("false\n");
+	}
+	return 0;
+}
---
+++ day4 q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("enter a number :");
+	scanf("%d",&num);
+	if(num>0){
+		printf("the number is positive\n");
+	}else if(num>0){
+		printf("the number is negative\n");	
+	}else{
+		printf("the number is zero\n");
+	}
+	return 0;
+}
